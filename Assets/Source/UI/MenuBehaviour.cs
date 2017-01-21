125d4cbf7d20b27ae3069a8c8e099447b18c2f71
Trylar666 <HellJumpers_ODTS99@hotmail.com>
2017-01-21 10:32:12 -0400

    Menu princpal
    Adicion del menu principal

---
+++ Assets/Source/UI/MenuBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuBehaviour : MonoBehaviour {
+
+	public Canvas QuitMenu;
+	public Button starText;
+	public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+
+		QuitMenu.GetComponent<Canvas>();
+		starText.GetComponent<Button>();
+		exitText.GetComponent<Button>();
+		QuitMenu.enabled = false;
+	}
+
+	public void exitPress(){
+
+		QuitMenu.enabled = true;
+		starText.enabled = false;
+		exitText.enabled = false;
+	}
+
+	public void noPress(){
+
+		QuitMenu.enabled = false;
+		starText.enabled = true;
+		exitText.enabled = true;
+	}
+
+
+	public void startLevel(){
+		//SceneManager.LoadScene(); cuando exista la primera escena colocar aqui
+	}
+
+	public void exitGame(){
+		Application.Quit();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
