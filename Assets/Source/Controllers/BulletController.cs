68b775de783da6619230e4bc55775afe78da7cf6
Trylar666 <HellJumpers_ODTS99@hotmail.com>
2017-01-21 14:30:10 -0400

    proyectil y menu principal

---
+++ Assets/Source/Controllers/BulletController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+	public float BulletDuration;
+	Rigidbody _rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		_rigidBody = gameObject.GetComponent<Rigidbody>();
+	}
+
+	public void Fire(Vector3 direction, float speed, float duration ){
+
+	_rigidBody.velocity = direction * speed;
+	BulletDuration = duration;
+	Destroy(gameObject, BulletDuration);
+
+	}
+}
