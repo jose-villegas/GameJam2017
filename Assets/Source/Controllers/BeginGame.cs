0c56b7fb4030d91862fb18878dff24169ac55314
José Villegas <villegasjose.gg@gmail.com>
2017-01-22 14:20:24 -0400

    commit

---
+++ Assets/Source/Controllers/BeginGame.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class BeginGame : MonoBehaviour {
+	
+	public void StartLevel(){
+		SceneManager.LoadScene(1); 
+	}
+}
